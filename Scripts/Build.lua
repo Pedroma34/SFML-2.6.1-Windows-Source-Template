aef72fa7a51a67c7241b754e098fe5cbeb96c3a5
Pedroma34 <62633760+Pedroma34@users.noreply.github.com>
2024-07-07 00:51:01 -0400

    Added support for mingw

--- Scripts/Build.lua
+++ Scripts/Build.lua
@@ -1,20 +1,23 @@
-workspace "SFML-Windows"
+workspace "SFML-Windows-2.6.1"
     architecture "x64"
-    configurations {"Debug", "Release"}
+    configurations {"Debug", "Release", "Dist"}
     startproject "System"
     startproject "App"
-    defines { "SFML_STATIC" } --builds the static version of SFML
     startproject "AppWindows"
+    defines { "SFML_STATIC" } --builds the static version of SFML
     --If action is for vs:
     filter "action:vs*"
         location "../VS2022" -- Location of the solution/project files
+    filter "action:gmake2"
+        defines { "UNICODE", "_UNICODE" } 
+        defines {"OPENAL_STATIC"}
     filter "system:windows"
         systemversion "latest"
 
 OutputDir = "%{cfg.buildcfg}"
 
-include "../AppWindows/Build-AppWindows.lua"
 include "../SFML/src/SFML/Graphics/Build-SFML-Graphics.lua"
 include "../SFML/src/SFML/System/Build-SFML-System.lua"
 include "../SFML/src/SFML/Window/Build-SFML-Window.lua"
 include "../SFML/src/SFML/Audio/Build-SFML-Audio.lua"
+include "../AppWindows/Build-AppWindows.lua"
